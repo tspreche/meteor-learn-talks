5d4b2a2fb9c8737f4f567cf12e5cc1018d30ba15
tspreche <tspreche@hotmail.com>
2014-03-29 08:26:05 +0100

    added Internationalization

---
+++ i18n.js
@@ -0,0 +1,16 @@
+i18n.map('en', {
+  delete: 'Delete',
+  upvote: 'Upvote',
+  downvote: 'Downvote',
+  by: 'by'
+});
+
+i18n.map('de', {
+  delete: 'Löschen',
+  upvote: '+1',
+  downvote: '-1',
+  by: 'von'
+});
+
+//i18n.setLanguage('en');
+i18n.setLanguage('de');
